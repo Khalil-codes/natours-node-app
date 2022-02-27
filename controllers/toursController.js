4704eca2b4a81bb70c6e4c04101a5e3ee581c298
khalil-codes <khalilpati55@gmail.com>
2022-02-28 03:18:51 +0530

    Added MongoDB operations to perform CRUD

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,102 +1,98 @@
-const fs = require("fs");
+const Tour = require("../models/toursModel");
 
-// File Stuff
-const tours = JSON.parse(
-    fs.readFileSync(`./dev-data/data/tours-simple.json`, "utf-8")
-);
-
-// Params middleware
-const checkID = (req, res, next, val) => {
-    const tour = tours.find((el) => el.id === +val);
-    if (!tour) {
-        res.status(400);
-        throw new Error("Tour Not Found");
+// @desc    Get tours
+// @route   GET /api/v1/tours
+// @access  Public
+const getTours = async (req, res) => {
+    try {
+        const tours = await Tour.find();
+        res.status(200).json({
+            status: "success",
+            results: tours.length,
+            data: { tours },
+        });
+    } catch {
+        res.status(404).json({
+            status: "fail",
+            message: "Something went wrong",
+        });
     }
-    next();
-};
-
-// Request Body Middleware
-const checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        res.status(400);
-        throw new Error("Name or Price field is missing");
-    }
-    next();
 };
 
 // @desc    Get tours
 // @route   GET /api/v1/tours
 // @access  Public
-const getTours = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        results: tours.length,
-        data: { tours },
-    });
-};
-
-// @desc    Get tours
-// @route   GET /api/v1/tours
-// @access  Public
-const getTourById = (req, res) => {
-    const tour = tours.find((el) => el.id === +req.params.id);
-
-    res.status(200).json({
-        status: "success",
-        data: { tour },
-    });
+const getTourById = async (req, res) => {
+    try {
+        const tour = await Tour.findById(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: { tour },
+        });
+    } catch {
+        res.status(404).json({
+            status: "fail",
+            message: "Tour not Found",
+        });
+    }
 };
 // @desc    Set tours
 // @route   POST /api/v1/tours
 // @access  Private
-const createTour = (req, res) => {
-    const newId = tours.at(-1).id + 1;
-    const newTour = { id: newId, ...req.body };
-    tours.push(newTour);
-    fs.writeFile(
-        "./dev-data/data/tours-simple.json",
-        JSON.stringify(tours),
-        (err) => {
-            if (err) {
-                res.status(400);
-                throw new Error("Something went Wrong in Adding the Data");
-            }
-            res.status(201).json({
-                status: "success",
-                data: {
-                    tour: newTour,
-                },
-            });
-        }
-    );
+const createTour = async (req, res) => {
+    try {
+        const tour = await Tour.create(req.body);
+        res.status(200).json({
+            status: "success",
+            data: { tour },
+        });
+    } catch {
+        res.status(404).json({
+            status: "fail",
+            message: "Invalid data sent!!",
+        });
+    }
 };
 
 // @desc    Update tours
 // @route   PATCH /api/v1/tours/:id
 // @access  Private
-const updateTour = (req, res) => {
-    const tour = tours.find((el) => el.id === +req.params.id);
-
-    res.status(200).json({
-        status: "success",
-        data: { tour: `<Updated Tour ${req.params.id}>` },
-    });
+const updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        res.status(200).json({
+            status: "success",
+            data: { tour },
+        });
+    } catch {
+        res.status(404).json({
+            status: "fail",
+            message: "Something went Wrong",
+        });
+    }
 };
 
 // @desc    Delete tours
 // @route   DELETE /api/v1/tours/:id
 // @access  Private
-const deleteTour = (req, res) => {
-    const tour = tours.find((el) => el.id === +req.params.id);
-    res.status(204).json({
-        status: "success",
-        data: null,
-    });
+const deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({
+            status: "success",
+            data: null,
+        });
+    } catch {
+        res.status(404).json({
+            status: "fail",
+            message: "Something went Wrong",
+        });
+    }
 };
 
 module.exports = {
-    checkBody,
-    checkID,
     getTours,
     createTour,
     updateTour,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,12 +6,9 @@ const {
     updateTour,
     deleteTour,
     getTourById,
-    checkID,
-    checkBody,
 } = require("../controllers/toursController");
 
-router.param("id", checkID);
-router.route("/").get(getTours).post(checkBody, createTour);
+router.route("/").get(getTours).post(createTour);
 router.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
