2372cbe192ccb19bf1687a79d2934104373a5629
khalil-codes <khalilpati55@gmail.com>
2022-02-28 03:28:19 +0530

    Added MongoDB operations to perform CRUD

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -61,6 +61,7 @@ const updateTour = async (req, res) => {
     try {
         const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
+            runValidators: true,
         });
         res.status(200).json({
             status: "success",
