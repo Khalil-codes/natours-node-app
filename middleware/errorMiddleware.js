b32e8f7b8994316b076ed5ed2be6e0492b83eefb
khalil-codes <khalilpati55@gmail.com>
2022-02-27 19:09:51 +0530

    Refactored JSON

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -2,6 +2,7 @@ const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 400;
     res.status(statusCode);
     res.json({
+        status: "fail",
         message: err.message,
         stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
     });
