9f4eb575c9386ad06421f8fe2ed7b41ac97f87b0
khalil-codes <khalilpati55@gmail.com>
2022-02-27 20:17:17 +0530

    Added middleware to serve static files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
 
 // Routes Middleware
 app.use("/api/v1/tours", require("./routes/tourRoutes"));
