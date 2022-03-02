4b35517aa4141a3a5234a2d4933fb12a66477cfa
khalil-codes <khalilpati55@gmail.com>
2022-03-03 01:57:05 +0530

    Added more fields to tourSchema

--- models/toursModel.js
+++ models/toursModel.js
@@ -1,16 +1,47 @@
 const mongoose = require("mongoose");
 
-const tourSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Please add Tour Name"],
-            unique: true,
-        },
-        rating: { type: Number, default: 4.5 },
-        price: { type: Number, required: [true, "Please add Tour Price"] },
+const tourSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add Tour Name"],
+        unique: true,
+        trim: true,
     },
-    { timestamps: true }
-);
+    duration: {
+        type: Number,
+        required: [true, "A Tour must have a duration"],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "A Tour must have a group size"],
+    },
+    difficulty: {
+        type: String,
+        required: [true, "A Tour must have a difficulty"],
+    },
+    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsQuantity: { type: Number, default: 0 },
+    price: { type: Number, required: [true, "Please add Tour Price"] },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, "A Tour must have a summary"],
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, "A Tour must have a cover Image"],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date],
+});
 
 module.exports = mongoose.model("Tour", tourSchema);
